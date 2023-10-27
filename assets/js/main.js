fdbae244cd76c282760c92633a40300a31fd7e9b
Lance Valle <vallelance8@gmail.com>
2023-10-27 11:51:32 +0800

    add nav bar show and hide menu functionality

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,23 @@
 /*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
 
+    //   MENU SHOW
+    // validate if constant exist
+    if(navToggle){
+        navToggle.addEventListener('click', ()=>{
+            navMenu.classList.add('show-menu')
+        })
+    }
+
+    // Menu Hidden
+    // validate if constant exist
+    if(navClose){
+        navClose.addEventListener('click', ()=>{
+            navMenu.classList.remove('show-menu')
+        })
+    }
 
 /*=============== REMOVE MENU MOBILE ===============*/
 
