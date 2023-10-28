c3cf1877f00c3cfe133cf7ad96baec0e895476e8
Lance Valle <vallelance8@gmail.com>
2023-10-28 17:31:38 +0800

    added html Home

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,7 +20,14 @@ const navMenu = document.getElementById('nav-menu'),
     }
 
 /*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll('.nav__link')
 
+const linkAction = () => {
+    const navMenu = document.getElementById("nav-menu")
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*=============== SWIPER PROJECTS ===============*/
 
